16b86b511da05d40882025cb30514a47da3c92c3
Alan Costa Facchini <costafacchini@gmail.com>
2024-06-08 22:08:39 -0300

    Use the Stock Ticker Finder service object on controller and return errors

--- app/controllers/v1/stock_report_controller.rb
+++ app/controllers/v1/stock_report_controller.rb
@@ -1,9 +1,12 @@
 class V1::StockReportController < ApplicationController
   def show
-    stock_ticker = Struct.new(:maximum_price, :minimum_price, :maximum_volume, :minimum_volume)
-                         .new(100.0, 50.0, 10.0, 6.0)
-    report = StockReport.new(stock_ticker)
+    stock_ticker = StockTickerFinder.new(params[:id]).call
+    if stock_ticker.success
+      report = StockReport.new(stock_ticker)
 
-    render json: report, status: :ok
+      render json: report, status: :ok
+    else
+      render json: { message: stock_ticker }, status: :not_found
+    end
   end
 end
--- spec/requests/v1/stock_report_spec.rb
+++ spec/requests/v1/stock_report_spec.rb
@@ -4,10 +4,33 @@ RSpec.describe "V1::StockReportController", type: :request do
   describe 'GET /show' do
     context 'when stock finder returns with success' do
       it 'renders a successful response' do
+        stock_ticker_finder = instance_double(StockTickerFinder)
+        allow(StockTickerFinder).to receive(:new).with('AABC').and_return(stock_ticker_finder)
+        allow(stock_ticker_finder).to receive(:call).and_return(Struct.new(:success, :maximum_price, :minimum_price,
+                                                                           :maximum_volume, :minimum_volume)
+                                                                      .new(true, 10.0, 3.0, 5.0, 1.2))
+
         get v1_stock_report_url('AABC')
+
+        expect(stock_ticker_finder).to have_received(:call)
 
         expect(response).to be_successful
         expect(response.body). to include 'report'
+      end
+    end
+
+    context 'when stock finder returns with success' do
+      it 'renders a error response' do
+        stock_ticker_finder = instance_double(StockTickerFinder)
+        allow(StockTickerFinder).to receive(:new).with('AABC').and_return(stock_ticker_finder)
+        allow(stock_ticker_finder).to receive(:call).and_return(Struct.new(:success, :error).new(false, 'some error'))
+
+        get v1_stock_report_url('AABC')
+
+        expect(stock_ticker_finder).to have_received(:call)
+
+        expect(response).to have_http_status(:not_found)
+        expect(response.body). to include 'some error'
       end
     end
   end
